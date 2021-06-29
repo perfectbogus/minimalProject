c9267a650c2b1dfd138def345a35dd8268a28297
eduardo escobar <war_army_10@hotmail.com>
2021-06-29 10:45:38 -0500

    First Commit for minimalProject"

---
+++ src/main/java/dev/perfectbogus/minimalproject/MinimalProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.perfectbogus.minimalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinimalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/perfectbogus/minimalproject/MinimalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.perfectbogus.minimalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
