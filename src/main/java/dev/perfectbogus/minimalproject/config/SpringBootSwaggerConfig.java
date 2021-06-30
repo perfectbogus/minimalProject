3eb61cea09b26a8534e8a20bf05a1db20b28278a
eduardo escobar <war_army_10@hotmail.com>
2021-06-29 21:14:19 -0500

    Adding Swagger Configuration working

---
+++ src/main/java/dev/perfectbogus/minimalproject/config/SpringBootSwaggerConfig.java
@@ -0,0 +1,23 @@
+package dev.perfectbogus.minimalproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringBootSwaggerConfig {
+
+    @Bean
+    public Docket documentation(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
--- src/main/java/dev/perfectbogus/minimalproject/repositories/QuoteRepository.java
+++ src/main/java/dev/perfectbogus/minimalproject/repositories/QuoteRepository.java
@@ -2,6 +2,8 @@ package dev.perfectbogus.minimalproject.repositories;
 
 import dev.perfectbogus.minimalproject.domain.Quote;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuoteRepository extends CrudRepository<Quote, Long > {
 }
